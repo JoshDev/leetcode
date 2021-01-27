caf74736c86f635acfaa889951652416db50fd5d
Josh Dev <1900843+JoshDev@users.noreply.github.com>
2021-01-26 22:08:54 -0500

    Q1365_HowManyNumbersAreSmallerThanTheCurrentNumber

---
+++ src/Q1365_HowManyNumbersAreSmallerThanTheCurrentNumber.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Q1365_HowManyNumbersAreSmallerThanTheCurrentNumber {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        int maxVal = nums[0];
+        for (int i : nums) {
+            freq.put(i, freq.getOrDefault(i, 0) + 1);
+            maxVal = Math.max(maxVal, i);
+        }
+        int[] testArr = new int[maxVal + 1];
+        testArr[0] = 0;
+        for (int i = 1; i <= maxVal; i++) {
+            testArr[i] = freq.getOrDefault(i - 1, 0) + testArr[i - 1];
+        }
+        int[] retArr = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            retArr[i] = testArr[nums[i]];
+        }
+        return retArr;
+    }
+}
