94a905f9c54ccb34ab1a985148369aa14e2baab2
Josh Dev <1900843+JoshDev@users.noreply.github.com>
2021-01-27 01:27:40 -0500

    Q1342_NumberOfStepsToReduceANumberToZero

---
+++ src/Q1342_NumberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,14 @@
+public class Q1342_NumberOfStepsToReduceANumberToZero {
+    public int numberOfSteps(int num) {
+        int count = 0;
+        while (num > 0) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num--;
+            }
+            count++;
+        }
+        return count;
+    }
+}
