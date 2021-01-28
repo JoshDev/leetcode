2e9e5593c1087697b0e429b16f54f0dacae3dc29
Josh Dev <1900843+JoshDev@users.noreply.github.com>
2021-01-28 01:32:38 -0500

    Q1662_CheckIfTwoStringArraysAreEquivalent

---
+++ src/Q1662_CheckIfTwoStringArraysAreEquivalent.java
@@ -0,0 +1,5 @@
+public class Q1662_CheckIfTwoStringArraysAreEquivalent {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        return String.join("", word1).equals(String.join("", word2));
+    }
+}
