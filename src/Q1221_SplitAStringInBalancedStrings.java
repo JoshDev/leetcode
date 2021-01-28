506981b9415cf722923777435709f69e0b2cda65
Josh Dev <1900843+JoshDev@users.noreply.github.com>
2021-01-28 01:10:27 -0500

    Q1221_SplitAStringInBalancedStrings

---
+++ src/Q1221_SplitAStringInBalancedStrings.java
@@ -0,0 +1,27 @@
+public class Q1221_SplitAStringInBalancedStrings {
+    public int balancedStringSplit(String s) {
+        int balancedCount = 0;
+        int rCount = 0, lCount = 0;
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                case 'R':
+                    rCount++;
+                    if (rCount == lCount) {
+                        balancedCount++;
+                        rCount = 0;
+                        lCount = 0;
+                    }
+                    break;
+                case 'L':
+                    lCount++;
+                    if (lCount == rCount) {
+                        balancedCount++;
+                        rCount = 0;
+                        lCount = 0;
+                    }
+                    break;
+            }
+        }
+        return balancedCount;
+    }
+}
