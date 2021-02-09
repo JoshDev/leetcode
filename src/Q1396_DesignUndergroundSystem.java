3b7dd8ffb7b65a03bcf22bc771bcdab3ddf790bd
Josh Dev <1900843+JoshDev@users.noreply.github.com>
2021-02-08 23:26:59 -0500

    Did some more problems

---
+++ src/Q1396_DesignUndergroundSystem.java
@@ -0,0 +1,31 @@
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Q1396_DesignUndergroundSystem {
+
+    Map<Integer, Map.Entry<String, Integer>> progressMap;
+    Map<Map.Entry<String, String>, Map.Entry<Integer, Integer>> totalCount;
+
+    public Q1396_DesignUndergroundSystem() {
+        progressMap = new HashMap<>();
+        totalCount = new HashMap<>();
+    }
+
+    public void checkIn(int id, String stationName, int t) {
+        progressMap.put(id, new AbstractMap.SimpleEntry<>(stationName, t));
+    }
+
+    public void checkOut(int id, String stationName, int t) {
+        Map.Entry<String, Integer> cameFrom = progressMap.get(id);
+        AbstractMap.SimpleEntry<String, String> stations = new AbstractMap.SimpleEntry<>(cameFrom.getKey(), stationName);
+        Map.Entry<Integer, Integer> currentTotal = totalCount.getOrDefault(stations, new AbstractMap.SimpleEntry<>(0, 0));
+        totalCount.put(stations, new AbstractMap.SimpleEntry<>(currentTotal.getKey() + 1, currentTotal.getValue() + (t - cameFrom.getValue())));
+    }
+
+    public double getAverageTime(String startStation, String endStation) {
+        Map.Entry<Integer, Integer> currentTotal = totalCount.getOrDefault(new AbstractMap.SimpleEntry<>(startStation, endStation), new AbstractMap.SimpleEntry<>(0, 0));
+        return currentTotal.getValue() * 1.0 / currentTotal.getKey();
+    }
+
+}
---
+++ src/Q394_DecodeString.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+public class Q394_DecodeString {
+    public String decodeString(String s) {
+        Stack<String> stringStack = new Stack<>();
+        StringBuilder finalString = new StringBuilder();
+        int i = 0;
+        while (i < s.length()) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                int endIndex = s.indexOf('[', i);
+                String substring = s.substring(i, endIndex);
+                stringStack.push(substring);
+                stringStack.push("[");
+                i += substring.length() + 1;
+            } else if (c == ']') {
+                StringBuilder insideParens = new StringBuilder();
+                while (!stringStack.peek().equals("[")) {
+                    String toBeAdded = stringStack.pop();
+                    insideParens.insert(0, toBeAdded);
+                }
+                stringStack.pop();
+                int times = Integer.parseInt(stringStack.pop());
+                while (times-- > 0) {
+                    stringStack.push(insideParens.toString());
+                }
+                i++;
+            } else {
+                stringStack.push(Character.toString(c));
+                i++;
+            }
+        }
+        StringBuilder end = new StringBuilder();
+        while (!stringStack.isEmpty()) {
+            end.insert(0, stringStack.pop());
+        }
+        finalString.append(end.toString());
+        return finalString.toString();
+    }
+}
